3ad3aea2bf40d99d2476b09052a297d802ee28d4
Nathan <fnathan@hotmail.fr>
2022-02-06 20:01:46 +0100

    navigation and fragments

---
+++ app/src/main/java/esgi/audiodb/MusicMainFragment.kt
@@ -0,0 +1,33 @@
+package esgi.audiodb
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.music_main.*
+
+class MusicMainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.music_main,
+            container,
+            false
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //link to bottom_nav
+        val navHost =
+            childFragmentManager.findFragmentById(R.id.music_main_nav_host) as NavHostFragment
+        NavigationUI.setupWithNavController(bottom_nav_view, navHost.navController)
+    }
+}
---
+++ app/src/main/java/esgi/audiodb/SearchFragment.kt
@@ -0,0 +1,24 @@
+package esgi.audiodb
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.w("on passe dans onCreate", "encore un");
+        return inflater.inflate(
+            R.layout.search,
+            container,
+            false
+        )
+    }
+}
